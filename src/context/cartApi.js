a2d242651a29a9f8c8f15d85b8fbbd0dfda95f8b
Ali <alichakri99@gmail.com>
2025-12-14 18:16:06 +0100

    commiting design fix #3

--- src/context/cartApi.js
+++ src/context/cartApi.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const CartApi = axios.create({
-    baseURL: "https://e-commerce-fitvibe-backend.onrender.com/user/cart",
+    baseURL: "https://e-commerce-fitvibe-backend.onrender.com/api/cart",
     withCredentials: true
 });
 
