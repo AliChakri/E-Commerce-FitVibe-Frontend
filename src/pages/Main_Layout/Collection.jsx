b97e0ce0cb55879bed8a704c153dc41b647777f0
Ali <alichakri99@gmail.com>
2026-01-07 12:17:05 +0100

    adding responsive#4

--- src/pages/Main_Layout/Collection.jsx
+++ src/pages/Main_Layout/Collection.jsx
@@ -205,7 +205,7 @@ const Collection = () => {
 <section className="min-h-screen w-full flex flex-col gap-8 bg-white dark:bg-gray-900 mx-auto mt-[8vh] overflow-hidden">
 
   {/* title section + Clear Button */}
-  <div className="flex items-center justify-between mb-6">
+  <div className="flex items-center justify-between my-6 p-4">
     <h2 className="text-3xl lg:text-4xl font-bold text-blue-600 dark:text-blue-400">{t("allProducts")}</h2>
 
     <div className="hidden md:flex items-center gap-3">
@@ -220,7 +220,7 @@ const Collection = () => {
   </div>
 
   {/* MAIN CONTENT */}
-  <div className="flex flex-col lg:flex-row gap-6">
+  <div className="flex flex-col lg:flex-row gap-6 p-4 lg:p-0 max-w-7xl mx-auto w-full">
 
     {/* --- FILTER SIDEBAR --- */}
     <aside className="lg:block w-full lg:w-[250px] 2xl:w-72 sticky top-20 self-start">
